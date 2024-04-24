510191d2ec100a70115819970d7beb818cf09dfd
divya370 <66016325+divya370@users.noreply.github.com>
2024-04-24 17:14:49 -0400

    Update search_functions.py

--- search_functions.py
+++ search_functions.py
@@ -95,7 +95,7 @@ class SearchInMongoDB:  ##Mayukh Sen
 
 ############################################################################################################################################################################
 class SearchCache: ##Divya Shah
-    def __init__(self, max_size=35):
+    def __init__(self, max_size=50):
         self.max_size = max_size
         self.heap = []
         self.cache = {}
